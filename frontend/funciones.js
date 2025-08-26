eacb63fee190ea2fe3c666961aa4ccb44b77bb2f
JosR <josedarealg@gmail.com>
2025-08-26 11:36:17 -0600

    Actualización con Página Inicial, diseño en general de botenes, reajustes en la interfaz del Sistema Solar Interactivo, adición de un encabezado, cuerpo y footer para la página de inicio. Se eliminan los cuadros de información de cada planeta giratorio para agregar más adelante el lista de todos los planetas para desplegar información detallada de cada uno y realizar la estructura y la base del panel de control.

--- frontend/funciones.js
+++ frontend/funciones.js
@@ -1,22 +1,3 @@
-// Bloques de información de cada planeta
-document.addEventListener('DOMContentLoaded', () => {
-  const infoBox = document.getElementById('infoBox');
-  const planetName = document.getElementById('planetName');
-  const planetInfo = document.getElementById('planetInfo');
-
-  const planets = document.querySelectorAll('.planet');
-
-  planets.forEach(planet => {
-    planet.addEventListener('click', () => {
-      const name = planet.getAttribute('data-name');
-      const info = planet.getAttribute('data-info');
-
-      planetName.textContent = name;
-      planetInfo.textContent = info;
-      infoBox.style.display = 'block';
-    });
-  });
-
   // Menú desplegable
   const hamburger = document.getElementById('hamburger');
   const sideMenu = document.getElementById('sideMenu');
